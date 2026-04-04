278f03b2ffaa89e5407d7082c0fe21f119616946
Saumrit <44732905+Saumrit@users.noreply.github.com>
2026-04-04 14:08:29 +0530

    Initialized current directory as Git repo

---
+++ src/test/java/com/saumrit/restclientdemo/RestclientdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumrit.restclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
